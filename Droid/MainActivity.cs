514917dc34731764a6060a0071f7852f3b7fa84d
viral7chauhan <viralchauhan@viralchauhan.local>
2017-11-28 15:32:00 +0530

    First commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,107 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using System;
+using Java.IO;
+using Android.Media;
+using Audacious.Droid.Utility;
+using Android.Content;
+
+namespace Audacious.Droid
+{
+    [Activity(Label = "Audacious", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+       
+        VcAudioRecorder mAudioRecorder;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.btnTap);
+
+            bool result = hasMicrophone();
+
+            if(result)
+            {
+                button.Visibility = Android.Views.ViewStates.Visible;
+            }
+            else
+            {
+                button.Visibility = Android.Views.ViewStates.Gone;
+                return;
+            }
+
+            mAudioRecorder = new VcAudioRecorder();
+
+            button.Click += delegate 
+            {
+                if(button.Text.Equals(Resources.GetString(Resource.String.StartRecording)))
+                {
+                    //Start Recording
+                    mAudioRecorder.mAudioFilePath = audioFilePath();
+                    mAudioRecorder.startRecording();
+
+                    button.Text = Resources.GetString(Resource.String.StopRecording);
+
+                }
+                else
+                {
+                    //Stop Recording
+                    mAudioRecorder.stopRecording();
+
+                    button.Text = Resources.GetString(Resource.String.StartRecording);
+                }
+            };
+
+        }
+
+
+        //Check feature availability
+        public bool hasMicrophone()
+        {
+            bool has = false;
+            try
+            {
+                has = PackageManager.HasSystemFeature(PackageManager.FeatureMicrophone);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("{0} - {1}, Exception: {2}", this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, ex.Message);
+            }
+            return has;
+        }
+
+        private string audioFilePath ()
+        {
+            string filePath = null;
+            try
+            {
+                File folder = new File(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/" + Resources.GetString(Resource.String.app_name));
+                //File folder = GetDir(Resources.GetString(Resource.String.app_name, FileCreationMode.Private));//new File(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/" + Resources.GetString(Resource.String.app_name));
+                bool result = false;
+                if (!folder.Exists())
+                {
+                    result = folder.Mkdir();
+                }
+
+                filePath = folder.Path + "/" + DateTime.Now.ToString() + ".3gp" ;
+                System.Diagnostics.Debug.WriteLine("filePath :{0} ", filePath);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("{0} - {1}, Exception: {2}", this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, ex.Message);
+            }
+            return filePath;
+        }
+
+    }
+}
+
---
+++ Droid/Utility/VcAudioRecorder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Android.Media;
+using Java.IO;
+
+namespace Audacious.Droid.Utility
+{
+    public class VcAudioRecorder
+    {
+
+        #region Properties
+
+        MediaRecorder mMediaRecorder;
+
+        public string mAudioFilePath;
+
+        #endregion
+
+
+        public VcAudioRecorder()
+        {
+                        
+        }
+
+        #region public
+
+        public void startRecording()
+        {
+            try
+            {
+                mediaRecorderReddy();
+                mMediaRecorder.Prepare();
+                mMediaRecorder.Start();
+            }
+            catch (IOException io)
+            {
+                io.PrintStackTrace();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("{0} - {1}, Exception: {2}", this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        public void stopRecording()
+        {
+            try
+            {
+                mMediaRecorder.Stop();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("{0} - {1}, Exception: {2}", this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        #endregion
+
+
+        private void mediaRecorderReddy ()
+        {
+            try
+            {
+                mMediaRecorder = new MediaRecorder();
+                mMediaRecorder.SetAudioSource(AudioSource.Mic);
+                mMediaRecorder.SetOutputFormat(OutputFormat.ThreeGpp);
+                mMediaRecorder.SetAudioEncoder(AudioEncoder.AmrNb);
+                mMediaRecorder.SetOutputFile(mAudioFilePath);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("{0} - {1}, Exception: {2}", this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+    }
+}
